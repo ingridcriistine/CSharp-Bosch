f6b1c7c5367a4f89f60603482f943093a224e525
ingridcriistine <ingridrochacristine@gmail.com>
2025-01-24 17:07:39 -0300

    updates

---
+++ Conversor de JSON/Converter.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Diagnostics.Metrics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Converter;
+
+public static class Converter
+{
+    // public static Task<string> ToJsonAsync<T>(this T obj)
+    // {
+
+    // }
+
+    public static string ToJson<T>(this T obj)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine("{");
+        toJson(obj, sb, 0);
+        sb.AppendLine("}");
+        return sb.ToString();
+    }
+
+    private static void toJson<T>(this T obj, StringBuilder sb, int tab)
+    {
+        var objeto = obj.GetType();
+
+        Console.WriteLine(objeto);
+
+        var props = objeto.GetProperties();
+
+        foreach (var item in props)
+        {
+            var propertyName = item.Name;
+            Console.WriteLine($"Atributo: {propertyName}");
+
+            var propertyValue = item.GetValue(obj);
+            Console.WriteLine($"Valor: {propertyValue}");
+            
+            if (propertyValue is IEnumerable e && item.PropertyType != typeof(string)) {
+                ident(sb, tab+1);
+                sb.Append($" '{propertyName}' : " + "[");
+                
+                foreach (var itemList in e) {
+                    sb.AppendLine("\t\t{");
+                    if (itemList.GetType().IsClass && itemList.GetType() != typeof(string)) {
+                        toJson(itemList, sb, tab);
+                        continue;
+                    }
+
+                     sb.AppendLine($"\t'{propertyName}' : '{itemList.ToString()}',");
+                    sb.AppendLine("\t\t}");
+                }
+ 
+                sb.AppendLine("]");
+            }
+
+            else if (item.PropertyType.IsClass && item.PropertyType != typeof(string)) {
+                toJson(propertyValue, sb, tab);
+            }
+
+            else
+            {
+                sb.AppendLine($"\t'{propertyName}' : '{propertyValue}',");
+            }
+        }
+    }
+
+    public static void ident(StringBuilder sb, int tab) {
+        sb.Append(new string('\t', tab));
+    }
+}
---
+++ Conversor de JSON/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection;
+using System.Text;
+using Converter;
+
+var obj = typeof(Empresa);
+
+var empresa = new Empresa(
+    "Bosch", 
+    new Funcionario(1, "Fábio", 100_000), 
+    [
+        new Funcionario(2, "Don", 50_000),
+        new Funcionario(3, "Queila", 50_000),
+        new Funcionario(4, "Trevis", 50_000),
+    ]
+);
+
+Console.WriteLine(Converter.Converter.ToJson(empresa));
+
+public record Funcionario(int Id, string Nome, decimal Salario);
+public record Empresa(string Nome, Funcionario Chefe, List<Funcionario> Funcionarios);
+
+
+
